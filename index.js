a1ad5b21ffd3fc8fe9fa5a5659397df3d918ee74
Jack Maunders <jmaunders92@gmail.com>
2020-12-04 17:19:13 +0000

    Update demo component

--- index.js
+++ index.js
@@ -1,64 +1,51 @@
-import * as React from "react"
-import './banner.css'
+import * as React from "react";
+import "./banner.css";
 
-const CTABanner = ({headline, subline, cta_text, cta_href, terms_summary, detailed_terms_href, textAlign, background_color, background_image_href}) => {
+const CTABanner = ({
+  headline,
+  subline,
+  cta_text,
+  cta_href,
+  terms_summary,
+  detailed_terms_href,
+  image_href,
+}) => {
+  return (
+    <section className="CTABanner">
+      <article className="Container">
+        <header>
+          <h1 className="Headline">{headline}</h1>
+          <h2 className="Subline">{subline}</h2>
 
-    return (
-        <div>
-            <section className="Section"  style={{backgroundImage: `url(${background_image_href})`, backgroundColor: background_color}}>
-                <article className="Container Landing" style={{  textAlign: textAlign}}>
-                    <header className="header">
-                        <div className="header2">
-                            <div className="content"/>
-                            <div>
-                                <h1 className="Headline">{headline}</h1>
-                            </div>
-                            <div>
-                                <h2 className="Subline">{subline}</h2>
-                            </div>
+          <a className="CTAButton" href={cta_href} color="#191414">
+            <span>{cta_text}</span>
+          </a>
 
-                            <div className="ButtonGroup">
-                                <div>
-                                    <a
-                                        className="CTAButton"
-                                        href={cta_href}
-                                        color="#191414"
-                                        tabindex="0"
-                                        rel="next"
-                                    >
-                                        <span>{cta_text}</span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </header>
-
-                    <footer className="FooterContainer">
-                        <div className="Footer">
-                            {terms_summary}{" "}
-                            {detailed_terms_href && (<><a href={detailed_terms_href}>Terms apply</a>.</>)}
-
-                        </div>
-                    </footer>
-                </article>
-            </section>
-        </div>
-    )
-}
-
+          <div className="Terms">
+            <p>
+              {terms_summary}{" "}
+              {detailed_terms_href && (
+                <a href={detailed_terms_href}>Terms apply.</a>
+              )}
+            </p>
+          </div>
+        </header>
+        <img src={image_href} alt="headphones" />
+      </article>
+    </section>
+  );
+};
 
 CTABanner.defaultProps = {
-    headline: 'Students 3 months of Premium for £0.99.',
-    subline: 'Only £4.99/month after. Cancel anytime',
-    cta_text: 'Get Premium',
-    cta_href: '/purchase/campaign/student',
-    terms_summary: 'Offer not available to users who already tried Premium. Offer ends 30 Jun 2019.',
-    detailed_terms_href: '/legal/student-intro-offer-terms-and-conditions',
-    background_image_href: 'https://campaigns.scdn.co/images/Desktop%20Asset%20-%20Student.jpg',
-    background_color: "#9487b4",
-    textAlign: "left"
+  headline: "Students 3 months of Premium for £0.99.",
+  subline: "Only £4.99/month after. Cancel anytime",
+  cta_text: "Get Premium",
+  cta_href: "/purchase/campaign/student",
+  terms_summary:
+    "Offer not available to users who already tried Premium. Offer ends 30 Jun 2019.",
+  detailed_terms_href: "/legal/student-intro-offer-terms-and-conditions",
+  image_href:
+    "https://campaigns.scdn.co/images/Desktop%20Asset%20-%20Student.jpg",
+};
 
-}
-
-
-export default CTABanner
+export default CTABanner;
